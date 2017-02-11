effdbe44b824d837381387ece310c278b575c701
Evan Rittenhouse <emanguy1@gmail.com>
2017-02-11 15:39:05 -0500

    Fix indentation, change /lists main attribute
     * /lists will now use the attribute "lists" rather than "listNames". It
      also returns the items in the lists now.

--- database.js
+++ database.js
@@ -33,86 +33,86 @@ module.exports = class DatabaseCommunicator
 	getLists()
 	{
 		return new Promise( (resolve, reject) =>
-				{
-					console.log("Querying the database");
+		{
+			console.log("Querying the database");
 
-					this._dbConnection.collection( "lists" ).distinct( "name", function(err, results)
-							{
-								if (err)
-								{
-									reject(err);
-								}
-								else
-								{
-									resolve(results);
-								}
-							});
-				});
+			this._dbConnection.collection( "lists" ).find({}, { _id: 0 }).toArray(function(err, results)
+			{
+				if (err)
+				{
+					reject(err);
+				}
+				else
+				{
+					resolve(results);
+				}
+			});
+		});
 	}
 
 	// Add a list
 	addList(listname)
 	{
 		var doQuery = new Promise( (resolve, reject) => 
+		{
+			this._dbConnection.collection("lists").find( { name: listname } ).count( (err, count) =>
+			{
+				if (err)
 				{
-					this._dbConnection.collection("lists").find( { name: listname } ).count( (err, count) =>
-							{
-								if (err)
-								{
-									console.log("Couldn't query the database");
-									reject(err);
-								}
-								else if (count > 0)
-								{
-									console.log("List name exists");
-									reject(new RangeError("A list with this name already exists"));
-								}
-								else
-								{
-									resolve(count);
-								}
-							});
-				});
+					console.log("Couldn't query the database");
+					reject(err);
+				}
+				else if (count > 0)
+				{
+					console.log("List name exists");
+					reject(new RangeError("A list with this name already exists"));
+				}
+				else
+				{
+					resolve(count);
+				}
+			});
+		});
 
 		return doQuery.then( (count) =>
+		{
+			return this._dbConnection.collection("lists").insertOne( { name: listname, items: [] }).then( (result) =>
+			{
+				return new Promise( (resolve, reject) => 
 				{
-					return this._dbConnection.collection("lists").insertOne( { name: listname, items: [] }).then( (result) =>
-							{
-								return new Promise( (resolve, reject) => 
-										{
-											if (result.insertedCount == 1)
-											{
-												console.log("Added new list: " + listname);
-												resolve("Successfully added a new list");
-											}
-											else
-											{
-												console.log("Inserted count was wrong: " + result.insertedCount);
-												reject(new Error("The database did not accept the change. This should not happpen."));
-											}
-										});
-							});
+					if (result.insertedCount == 1)
+					{
+						console.log("Added new list: " + listname);
+						resolve("Successfully added a new list");
+					}
+					else
+					{
+						console.log("Inserted count was wrong: " + result.insertedCount);
+						reject(new Error("The database did not accept the change. This should not happpen."));
+					}
 				});
+			});
+		});
 	}
 
 	// Remove a list
 	removeList(listname)
 	{
 		return this._dbConnection.collection("lists").deleteOne( {name: listname} ).then( (result) =>
+		{
+			return new Promise( (resolve, reject) =>
+			{
+				if (result.deletedCount == 1)
 				{
-					return new Promise( (resolve, reject) =>
-							{
-								if (result.deletedCount == 1)
-								{
-									console.log("Successfully deleted list " + listname);
-									resolve("Successfully removed the list.");
-								}
-								else
-								{
-									console.log("Deleted count was wrong: " + result.deletedCount);
-									reject(new Error( "There was not a list with the specified name." ));
-								}
-							});	
-				});
+					console.log("Successfully deleted list " + listname);
+					resolve("Successfully removed the list.");
+				}
+				else
+				{
+					console.log("Deleted count was wrong: " + result.deletedCount);
+					reject(new Error( "There was not a list with the specified name." ));
+				}
+			});	
+		});
 	}
 }
--- index.js
+++ index.js
@@ -5,62 +5,62 @@ var databaseClass = require("./database");
 var database = new databaseClass();
 
 database.connect("mongodb://localhost:27017/listdb").then(() =>
+{
+	console.log("Received a database object!");
+
+	listApp.disable("x-powered-by");
+
+	listApp.get("/lists", (req, res) =>
+	{
+		database.getLists().then( (results) =>
 		{
-			console.log("Received a database object!");
+			console.log("Returned list results");
 
-			listApp.disable("x-powered-by");
+			var resultObj = {
+				lists: results
+			};
 
-			listApp.get("/lists", (req, res) =>
-					{
-						database.getLists().then( (results) =>
-								{
-									console.log("Returned list results");
-
-									var resultObj = {
-										listNames : results
-									};
-
-									res.json(resultObj);
-								}).catch( (err) =>
-								{
-									console.log("Failed to connect to DB");
-									res.status(503).json({ err: "Lost connection to the database." });
-								});
-					});
-
-			listApp.put("/add/:listname", (req, res) =>
-					{
-						var listname = req.params.listname;
-						
-						database.addList(listname).then( (result) =>
-								{
-									var response = { message: result };
-									res.json(response);
-								}).catch( (err) =>
-								{
-									var response = { error: err.message };
-									res.status(400).json(response);
-								});
-					});
-
-			listApp.put("/remove/:listname", (req, res) =>
-					{
-						var listname = req.params.listname;
-
-						database.removeList(listname).then( (result) =>
-								{
-									var response = { message: result };
-									res.json(response);
-								}).catch( (err) =>
-								{
-									var response = { error: err.message };
-									res.status(400).json(response);
-								});
-					})
-
-			listApp.listen(3000);
-		})
-	.catch((err) =>
+			res.json(resultObj);
+		}).catch( (err) =>
 		{
-			console.log("Rejected with error");
+			console.log("Failed to connect to DB");
+			res.status(503).json({ err: "Lost connection to the database." });
 		});
+	});
+
+	listApp.put("/add/:listname", (req, res) =>
+	{
+		var listname = req.params.listname;
+		
+		database.addList(listname).then( (result) =>
+		{
+			var response = { message: result };
+			res.json(response);
+		}).catch( (err) =>
+		{
+			var response = { error: err.message };
+			res.status(400).json(response);
+		});
+	});
+
+	listApp.put("/remove/:listname", (req, res) =>
+	{
+		var listname = req.params.listname;
+
+		database.removeList(listname).then( (result) =>
+		{
+			var response = { message: result };
+			res.json(response);
+		}).catch( (err) =>
+		{
+			var response = { error: err.message };
+			res.status(400).json(response);
+		});
+	})
+
+	listApp.listen(3000);
+})
+.catch((err) =>
+{
+	console.log("Rejected with error");
+});
